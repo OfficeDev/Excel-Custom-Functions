a8f6d19bbe6c49b6a916a46b3b79ac2a96f45920
Courtney Owen <cowen@microsoft.com>
2018-09-04 14:27:37 -0700

    update pack.json nativeBundles settings; remove unneeded file

--- haul.config.js
+++ haul.config.js
@@ -1,6 +1,6 @@
 const resources = require('./scripts/haul-resources');
 const haulConfigOptions = {
-    "entryFile": "./functions.ts"
+    "entryFile": "./src/index.ts"
 }
 
 export default resources.createHaulConfig(haulConfigOptions);
