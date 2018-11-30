6aaa408e96198e27b19454bd66974a69f379580b
Adam Krantz <Adam.Krantz@microsoft.com>
2018-11-30 08:13:37 -0800

    load functions.html from dist folder
    If the functions.html is loaded from webpack-dev-server using url https://localhost:3000/functions.html,
    it encounters errors. This appears to be because webpack-dev-server assumes a browser environment but
    custom functions are run directly using the JavaScript runtime.
    
    To work around this, use the dist folder for the custom function urls (page, metadata, and script):
    page:     https://localhost:3000/dist/functions.html
    metadata: https://localhost:3000/dist/functions.json
    script:   https://localhost:3000/dist/functions.js
    
    The taskpane which runs in the browser WebView control is still loaded using webpack-dev-server
    (https://localhost:3000/taskpane.html).

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,69 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const fs = require("fs");
 const webpack = require("webpack");
 
 module.exports = (env, options) => {
-    const dev = options.mode === "development";
-    const config = {
-        devtool: 'source-map',
-        entry: {
-            taskpane: './src/taskpane/taskpane.ts',
-            functions: './src/functions/functions.ts'
+  const dev = options.mode === "development";
+  const config = {
+    devtool: "source-map",
+    entry: {
+        functions: "./src/functions/functions.ts",
+        taskpane: "./src/taskpane/taskpane.ts"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader"
         },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.html', '.js']
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: "html-loader"
         },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    exclude: /node_modules/,
-                    use: 'ts-loader'
-                },
-                {
-                    test: /\.html$/,
-                    exclude: /node_modules/,
-                    use: 'html-loader'
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif)$/,
-                    use: 'file-loader'
-                }
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(['dist']),
-            new CopyWebpackPlugin([
-                {
-                    to: 'functions.json',
-                    from: './src/functions/functions.json'
-                }
-            ]),
-            new HtmlWebpackPlugin({
-                filename: 'taskpane.html',
-                template: './src/taskpane/taskpane.html',
-                chunks: ['taskpane']
-            }),
-            new HtmlWebpackPlugin({
-                filename: 'functions.html',
-                template: './src/functions/functions.html',
-                chunks: ['functions']
-            }),
-            new webpack.ProvidePlugin({
-                Promise: ['es6-promise', 'Promise']
-            })
-        ],
-        devServer: {
-            
-            https: {
-              key: fs.readFileSync('./certs/server.key'),
-              cert: fs.readFileSync('./certs/server.crt'),
-              ca: fs.readFileSync('./certs/ca.crt'),
-            },
-            port: 3000
-        }        
-    };
-    
-    return config;
-}
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          use: "file-loader"
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(dev ? [] : ["dist"]),
+      new CopyWebpackPlugin([
+        {
+          to: "functions.json",
+          from: "./src/functions/functions.json"
+        }
+      ]),
+      new HtmlWebpackPlugin({
+        filename: "functions.html",
+        template: "./src/functions/functions.html",
+        chunks: ["functions"]
+      }),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: "./src/taskpane/taskpane.html",
+        chunks: ["taskpane"]
+      }),
+      new webpack.ProvidePlugin({
+        Promise: ["es6-promise", "Promise"]
+      })
+    ],
+    devServer: {
+      https: {
+        key: fs.readFileSync("./certs/server.key"),
+        cert: fs.readFileSync("./certs/server.crt"),
+        ca: fs.readFileSync("./certs/ca.crt")
+      },
+      port: 3000
+    }
+  };
+
+  return config;
+};
