ec4d0dcd9f200853ac0f84d0943ae11330b20d5a
Adam Krantz <Adam.Krantz@microsoft.com>
2018-12-05 18:27:43 -0800

    Debug custom functions in Excel Desktop on Windows using VS Code (TypeScript) (#65)
    Package.json contains packages and scripts to enable debugging of custom functions in Excel. VS Code tasks and launch configuration are defined. 
    
    To debug custom functions in Excel Desktop on Windows using VS Code, do the following:
    1. Open the folder in VS Code.
    2. From the Terminal menu, select Run Task... and choose Install.
    3. From the Terminal menu, select Run Task... and choose Build (Development).
    4. Open the functions.ts file and set any desired breakpoints. For example, on line 2 in the add() function.
    5. Start Debugging by doing one of the following: 
        * Press F5, 
        * From the Debug menu, choose Start Debugging.
        * Switch to the Debug view, and press the Green triangle (play) button.
    6. When Excel is opened, in any cell, enter a formula that uses the function. For example: =CONTOSO.ADD(1, 2). The breakpoint should be hit.
    
    To stop debugging, close the Excel document.

---
+++ src/functions/functions.ts
@@ -0,0 +1,71 @@
+﻿/**
+ * Adds two numbers.
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+function add(first: number, second: number): number {
+  return first + second;
+}
+
+/**
+ * Displays the current time once a second.
+ * @param handler Custom function handler  
+ */
+function clock(handler: CustomFunctions.StreamingHandler<string>): void {
+  const timer = setInterval(() => {
+    const time = currentTime();
+    handler.setResult(time);
+  }, 1000);
+
+  handler.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Returns the current time.
+ * @returns String with the current time formatted for the current locale.
+ */
+function currentTime(): string {
+  return new Date().toLocaleTimeString();
+}
+
+/**
+ * Increments a value once a second.
+ * @param incrementBy Amount to increment
+ * @param handler Custom function handler 
+ */
+function increment(incrementBy: number, handler: CustomFunctions.StreamingHandler<number>): void {
+  let result = 0;
+  const timer = setInterval(() => {
+    result += incrementBy;
+    handler.setResult(result);
+  }, 1000);
+
+  handler.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Writes a message to console.log().
+ * @param message String to write.
+ * @returns String to write.
+ */
+function logMessage(message: string): string {
+  console.log(message);
+
+  return message;
+}
+
+/**
+ * Defines the implementation of the custom functions
+ * for the function id defined in the metadata file (functions.json).
+ */
+if (typeof(CustomFunctionMappings) !== "undefined") {
+  CustomFunctionMappings.ADD = add;
+  CustomFunctionMappings.CLOCK = clock;
+  CustomFunctionMappings.INCREMENT = increment;
+  CustomFunctionMappings.LOG = logMessage;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,69 @@
-const path = require('path');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require("fs");
+const webpack = require("webpack");
 
-module.exports = {
-    mode: 'development',
-    entry: './src/customfunctions.js',
-    output: {
-        path: path.resolve(__dirname, 'dist/win32/ship'),
-        filename: 'index.win32.bundle'
-    },
+module.exports = (env, options) => {
+  const dev = options.mode === "development";
+  const config = {
     devtool: "source-map",
-    resolve: {
-        extensions: ['.js', 'json']
+    entry: {
+        functions: "./src/functions/functions.ts",
+        taskpane: "./src/taskpane/taskpane.ts"
     },
-    devServer: {
-        port: 8081,
-        hot: true,
-        inline: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader"
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: "html-loader"
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          use: "file-loader"
         }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(dev ? [] : ["dist"]),
+      new CopyWebpackPlugin([
+        {
+          to: "functions.json",
+          from: "./src/functions/functions.json"
+        }
+      ]),
+      new HtmlWebpackPlugin({
+        filename: "functions.html",
+        template: "./src/functions/functions.html",
+        chunks: ["functions"]
+      }),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: "./src/taskpane/taskpane.html",
+        chunks: ["taskpane"]
+      }),
+      new webpack.ProvidePlugin({
+        Promise: ["es6-promise", "Promise"]
+      })
+    ],
+    devServer: {
+      https: {
+        key: fs.readFileSync("./certs/server.key"),
+        cert: fs.readFileSync("./certs/server.crt"),
+        ca: fs.readFileSync("./certs/ca.crt")
+      },
+      port: 3000
     }
+  };
+
+  return config;
 };
