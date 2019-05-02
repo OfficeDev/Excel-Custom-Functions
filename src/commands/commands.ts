9c8796f41220f7c6b942fde3a8b624a91176f7d2
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-02 08:27:09 -0700

    Add Outlook add-in command that invokes a function

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,11 +3,37 @@
  * See LICENSE in the project root for license information.
  */
 
-(() => {
-  // The initialize function must be run each time a new page is loaded
-  Office.initialize = () => {
+Office.onReady(info => {
+  // If needed, Office.js is ready to be called
+});
 
-  };
+/**
+ * Shows a notification when the add-in command is executed.
+ * @param event 
+ */
+function action(event: Office.AddinCommands.Event) {
+  const message: Office.NotificationMessageDetails = {
+    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage, 
+    message: "Performed action.",
+    icon: "Icon.80x80",
+    persistent: true
+  }
+  
+  // Show a notification message
+  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
 
-  // Add any ui-less function here
-})();
+  // Be sure to indicate when the add-in command function is complete
+  event.completed();
+}
+
+function getGlobal() { 
+  return (typeof self !== "undefined") ? self :
+    (typeof window !== "undefined") ? window : 
+    (typeof global !== "undefined") ? global :
+    undefined;
+}
+
+const g = getGlobal() as any;
+
+// the add-in command functions need to be available in global scope
+g.action = action;
