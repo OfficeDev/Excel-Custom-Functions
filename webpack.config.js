e02646a8ffcccf3412a3be62ccf26c3269dd08ba
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-31 09:22:14 -0700

    update import for CleanWebpackPlugin@3.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const devCerts = require("office-addin-dev-certs");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const fs = require("fs");
