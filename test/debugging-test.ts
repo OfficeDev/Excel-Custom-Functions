0e6a3fd52056fd73bfb174e53a0c9168a9d12d2c
igor-ribeiiro <igormr98mr@gmail.com>
2021-05-12 15:09:08 -0700

    Using different devserver port

--- test/debugging-test.ts
+++ test/debugging-test.ts
@@ -10,8 +10,6 @@ import * as path from "path";
 const WebSocket = require("ws");
 const host: string = "excel";
 const manifestPath = path.resolve(`${process.cwd()}/test/manifests/test-manifest-debugging.xml`);
-const port: number = 4201;
-const testServer = new officeAddinTestServer.TestServer(port);
 
 async function sleep(ms: number): Promise<any> {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -30,39 +28,25 @@ function sendWebsocketMessage(method : string) {
 describe("Test Excel Custom Functions", function () {
     before(`Setup test environment and sideload ${host}`, async function () {
         this.timeout(0);
-
         // Call startDebugging to start dev-server and sideload
-        const devServerCmd = `npm run dev-server -- --config ./test/webpack.config.js`;
-        const devServerPort = parseNumber(process.env.npm_package_config_dev_server_port || 3000);
+        const devServerCmd = `npm run dev-server -- --config ./test/webpack-debugging.config.js`;
+        const devServerPort = parseNumber(3001);
         const options = { 
             appType: AppType.Desktop, 
             app: toOfficeApp(host), 
             devServerCommandLine: devServerCmd, 
             devServerPort: devServerPort, 
-            enableDebugging: true
+            enableDebugging: true // Put true here, false just for testing the tester
         };
         await startDebugging(manifestPath, options);
     });
     describe("Test Debugger", function () {
         before("Open websocket connection to Debugger", async function () {
             this.timeout(12000);
-
-            this.timeout(0);
             await sleep(10000);
             const url = 'ws://localhost:9229/runtime1';
             ws = new WebSocket(url);
             await sleep(1000);
-
-            ws.on('connection', (ws) => {
-                websocketOpened = true;
-                console.log("");
-                console.log("");
-                console.log("");
-                console.log("");
-                console.log("");
-                console.log("");
-                console.log("oiii");
-            });
 
             let websocketOpened = false;
             for(let i = 0; i < 10 && !websocketOpened; i ++) {
@@ -74,8 +58,6 @@ describe("Test Excel Custom Functions", function () {
             });
 
             ws.on('message', (data) => {
-                console.log("data = ");
-                console.log(data);
                 assert.equal(JSON.parse(data).error, undefined);
             });
         }),
