eb08be7e8e4ccb2c80ecfcd3ea4b5f53a817272d
Darren Miller <darrmill@microsoft.com>
2021-06-30 16:01:32 -0700

    Update webpack versions

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -4,76 +4,81 @@ const devCerts = require("office-addin-dev-certs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = async (env, options) => {
-    // const dev = options.mode === "development";
-    const config = {
-        devtool: "source-map",
-        entry: {
-            polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-            test: path.resolve(__dirname, './src/test-taskpane.ts')
-        },
-        output: { path: path.resolve(__dirname, "testBuild") },
-        resolve: {
-            extensions: [".ts", ".tsx", ".html", ".js"]
-        },
-        node: {
-            child_process: 'empty',
-            fs: 'empty'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    exclude: /node_modules/,
-                    use: {
-                      loader: "babel-loader",
-                      options: {
-                        presets: ['@babel/preset-typescript']
-                      }
-                    }
-                },
-                {
-                    test: /\.tsx?$/,
-                    exclude: /node_modules/,
-                    use: "ts-loader"
-                },
-                {
-                    test: /\.html$/,
-                    exclude: /node_modules/,
-                    use: "html-loader"
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif)$/,
-                    use: "file-loader"
-                }
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                filename: "taskpane.html",
-                template: path.resolve(__dirname, './src/test-taskpane.html'),
-                chunks: ["polyfill", "test"]
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                {
-                    to: "taskpane.css",
-                    from: path.resolve(__dirname, './../src/taskpane/taskpane.css')
-                }
-                ]}),
-        ],
-        devServer: {
-            contentBase: path.join(__dirname, 'testBuild'),
-            headers: {
-                "Access-Control-Allow-Origin": "*"
+  // const dev = options.mode === "development";
+  const config = {
+    devtool: "source-map",
+    entry: {
+      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+      test: path.resolve(__dirname, "./src/test-taskpane.ts"),
+    },
+    output: {
+      path: path.resolve(__dirname, "testBuild"),
+      publicPath: "",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"],
+      fallback: {
+        child_process: false,
+        fs: false,
+        os: false,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-typescript"],
             },
-            https: (options.https !== undefined) ? options.https : await devCerts.getHttpsServerOptions(),
-            port: process.env.npm_package_config_dev_server_port || 3000
-        }
-    };
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: "html-loader",
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          use: "file-loader",
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: path.resolve(__dirname, "./src/test-taskpane.html"),
+        chunks: ["polyfill", "test"],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "./../src/taskpane/taskpane.css"),
+            to: "taskpane.css",
+          },
+        ],
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "testBuild"),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      https: options.https !== undefined ? options.https : await devCerts.getHttpsServerOptions(),
+      port: process.env.npm_package_config_dev_server_port || 3000,
+    },
+  };
 
-    return config;
+  return config;
 };
