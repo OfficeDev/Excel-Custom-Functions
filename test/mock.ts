bec7561d61be37b6f1659621e713d2fa1e0d43c4
Igor Ribeiro <igormr98mr@gmail.com>
2021-08-09 11:45:23 -0700

    Adding mock file

---
+++ test/mock.ts
@@ -0,0 +1,37 @@
+/// <reference path="../node_modules/@types/office-js/index.d.ts" />
+
+import * as sinon from 'sinon';
+import { Office, Excel, OfficeExtension } from '@microsoft/office-js/dist/excel.js';
+// eslint-disable-next-line no-undef
+global.Excel = Excel;
+// eslint-disable-next-line no-undef
+global.OfficeExtension = OfficeExtension;
+
+OfficeExtension.TestUtility.setMock(true);
+
+import * as assert from "assert";
+
+async function getSelectedRangeAddress(context: Excel.RequestContext): Promise<string> {
+  const range: Excel.Range = context.workbook.getSelectedRange();
+
+  range.load("address");
+  //await context.sync();
+
+  return range.address;
+}
+
+// eslint-disable-next-line no-undef
+describe(`Test Task Pane Project mocking`, function () {
+  // eslint-disable-next-line no-undef
+  it("Validate mockt, async function()", async function () {
+    const context: Excel.RequestContext = new Excel.RequestContext();
+    const range: Excel.Range = context.workbook.getSelectedRange();
+
+    range.setMockData({
+      address: "C2",
+    });
+    sinon.stub(context.workbook, "getSelectedRange").callsFake(() => range);
+
+    assert.strictEqual(await getSelectedRangeAddress(context), "C2");
+  });
+});
