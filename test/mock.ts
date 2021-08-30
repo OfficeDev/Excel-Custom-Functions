116ac8d9f5f25edf44f9bde9401ff36857f98995
Igor Ribeiro <igormr98mr@gmail.com>
2021-08-30 11:43:25 -0700

    Making default mock be available again

--- test/mock.ts
+++ test/mock.ts
@@ -1,55 +1,55 @@
-// import * as sinon from "sinon";
-// import { Excel, OfficeExtension } from "@microsoft/office-js/dist/excel.js";
-// import { getSelectedRangeAddressOtherFile } from "../src/test-file";
+import * as sinon from "sinon";
+import { Excel, OfficeExtension } from "@microsoft/office-js/dist/excel.js";
+import { getSelectedRangeAddressOtherFile } from "../src/test-file";
 
-// OfficeExtension.TestUtility.setMock(true);
+OfficeExtension.TestUtility.setMock(true);
 
-// import * as assert from "assert";
+import * as assert from "assert";
 
-// async function getSelectedRangeAddress(context: Excel.RequestContext): Promise<string> {
-//   const range: Excel.Range = context.workbook.getSelectedRange();
+async function getSelectedRangeAddress(context: Excel.RequestContext): Promise<string> {
+  const range: Excel.Range = context.workbook.getSelectedRange();
 
-//   range.load("address");
-//   await context.sync();
+  range.load("address");
+  await context.sync();
 
-//   return range.address;
-// }
+  return range.address;
+}
 
-// /* global before, it, global */
+/* global before, it, global */
 
-// // eslint-disable-next-line no-undef
-// describe(`Test Task Pane Project mocking`, function () {
-//   before("Setup global variable", function () {
-//     global.Excel = Excel;
-//   });
-//   it("Validate mock within same file using enlistment excel", async function () {
-//     const context: Excel.RequestContext = new Excel.RequestContext();
-//     const range: Excel.Range = context.workbook.getSelectedRange();
+// eslint-disable-next-line no-undef
+describe(`Test Task Pane Project mocking`, function () {
+  before("Setup global variable", function () {
+    global.Excel = Excel;
+  });
+  it("Validate mock within same file using enlistment excel", async function () {
+    const context: Excel.RequestContext = new Excel.RequestContext();
+    const range: Excel.Range = context.workbook.getSelectedRange();
 
-//     range.setMockData({
-//       address: "C2",
-//     });
-//     sinon.stub(context.workbook, "getSelectedRange").callsFake(() => range);
+    range.setMockData({
+      address: "C2",
+    });
+    sinon.stub(context.workbook, "getSelectedRange").callsFake(() => range);
 
-//     const contextSyncSpy = sinon.spy(context, "sync");
-//     const loadSpy = sinon.spy(range, "load");
+    const contextSyncSpy = sinon.spy(context, "sync");
+    const loadSpy = sinon.spy(range, "load");
 
-//     assert.strictEqual(await getSelectedRangeAddress(context), "C2");
-//     assert(contextSyncSpy.calledOnce);
-//     assert(loadSpy.withArgs("address").calledOnce);
-//   });
-//   it("Validate mock within different file using enlistment excel", async function () {
-//     const context: Excel.RequestContext = new Excel.RequestContext();
-//     const range: Excel.Range = context.workbook.getSelectedRange();
-//     range.setMockData({
-//       address: "C2",
-//     });
-//     sinon.stub(context.workbook, "getSelectedRange").callsFake(() => range);
-//     const contextSyncSpy = sinon.spy(context, "sync");
-//     const loadSpy = sinon.spy(range, "load");
+    assert.strictEqual(await getSelectedRangeAddress(context), "C2");
+    assert(contextSyncSpy.calledOnce);
+    assert(loadSpy.withArgs("address").calledOnce);
+  });
+  it("Validate mock within different file using enlistment excel", async function () {
+    const context: Excel.RequestContext = new Excel.RequestContext();
+    const range: Excel.Range = context.workbook.getSelectedRange();
+    range.setMockData({
+      address: "C2",
+    });
+    sinon.stub(context.workbook, "getSelectedRange").callsFake(() => range);
+    const contextSyncSpy = sinon.spy(context, "sync");
+    const loadSpy = sinon.spy(range, "load");
 
-//     assert.strictEqual(await getSelectedRangeAddressOtherFile(context), "C2");
-//     assert(contextSyncSpy.calledOnce);
-//     assert(loadSpy.withArgs("address").calledOnce);
-//   });
-// });
+    assert.strictEqual(await getSelectedRangeAddressOtherFile(context), "C2");
+    assert(contextSyncSpy.calledOnce);
+    assert(loadSpy.withArgs("address").calledOnce);
+  });
+});
