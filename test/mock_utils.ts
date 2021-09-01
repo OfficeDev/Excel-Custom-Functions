3c3d3b09de97eda11363f9310b95ef71b537b48f
Igor Ribeiro <igormr98mr@gmail.com>
2021-09-01 15:59:12 -0700

    Deleting useless classes

--- test/mock_utils.ts
+++ test/mock_utils.ts
@@ -26,14 +26,6 @@ export class OfficeJSMock {
     }
   }
 
-  _assignValue(propertyName: string) {
-    if (this.properties.get(propertyName).isObject) {
-      this[propertyName] = this.properties.get(propertyName);
-    } else {
-      this[propertyName] = this.properties.get(propertyName).value;
-    }
-  }
-
   addMockFunction(methodName: string, functionality?: Function) {
     this[methodName] = functionality ? functionality : function () {}
   }
@@ -41,10 +33,6 @@ export class OfficeJSMock {
   addMockObject(objectName: string) {
     this.properties.set(objectName, new OfficeJSMock(objectName, true));
     this[objectName] = this.properties.get(objectName);
-  }
-
-  _setValue(value: string) {
-    this.valueBeforeLoaded = value;
   }
 
   setMock(propertyName: string, value: string) {
@@ -55,97 +43,22 @@ export class OfficeJSMock {
     }
   }
 
-  properties: Map<string, OfficeJSMock>;
+  _assignValue(propertyName: string) {
+    if (this.properties.get(propertyName).isObject) {
+      this[propertyName] = this.properties.get(propertyName);
+    } else {
+      this[propertyName] = this.properties.get(propertyName).value;
+    }
+  }
 
+  _setValue(value: string) {
+    this.valueBeforeLoaded = value;
+  }
+
+  properties: Map<string, OfficeJSMock>;
   loaded: boolean;
   name: string;
   value: string;
   valueBeforeLoaded: string;
   isObject: boolean;
-}
-
-class Property {
-  constructor(name: string) {
-    this.loaded = false;
-    this.name = name;
-    this.value = `Error, ${name} was not loaded`;
-  }
-
-  load() {
-    this.loaded = true;
-    this.value = `Error, context.sync() was not called`;
-  }
-
-  sync() {
-    if (this.loaded) {
-      this.value = this.valueBeforeLoaded;
-    }
-  }
-
-  setMock(value: string) {
-    this.valueBeforeLoaded = value;
-  }
-
-  loaded: boolean;
-  name: string;
-  value: string;
-  valueBeforeLoaded: string;
-}
-
-
-export class RangeMock extends OfficeJSMock {
-
-}
-
-
-export class FillMock {
-  color: string;
-}
-
-class FormatMock {
-  constructor() {
-    this.fill = new FillMock();
-  }
-  fill: FillMock;
-}
-
-
-
-
-
-
-
-class Range2 extends RangeMock {
-
-}
-
-export class WorkbookMock {
-  constructor() {
-    this.range = new RangeMock();
-  }
-  getSelectedRange(): RangeMock {
-    return this.range;
-  }
-  sync(): void {
-    this.range.sync();
-  }
-  range: RangeMock;
-}
-
-export class ContextMock {
-  constructor() {
-    this.workbook = new WorkbookMock();
-  }
-  async sync(): Promise<void> {
-    this.workbook.sync();
-  }
-  workbook: WorkbookMock;
-}
-
-export class ExcelMock {
-  async run(callback): Promise<void> {
-    this.context = new ContextMock();
-    await callback(this.context);
-  }
-  context: ContextMock;
 }
--- test/mock_with_no_imports.ts
+++ test/mock_with_no_imports.ts
@@ -1,25 +1,39 @@
 import * as assert from "assert";
 import * as sinon from "sinon";
 
-import { ContextMock, ExcelMock, OfficeJSMock } from "./mock_utils";
+import { OfficeJSMock } from "./mock_utils";
 import { getSelectedRangeAddress, run } from "../src/test-file";
 
 /* global describe, global, it */
 
 describe(`Test Task Pane Project mocking without imports`, function () {
   it("getSelectedRangeAddress", async function () {
-    const context = new OfficeJSMock("context") as any;
+    const contextMock = new OfficeJSMock("context") as any;
 
-    context.addMockObject("workbook");
-    context.workbook.addMockObject("range");    
-    context.workbook.addMockFunction("getSelectedRange", () => context.workbook.range);
+    contextMock.addMockObject("workbook");
+    contextMock.workbook.addMockObject("range");    
+    contextMock.workbook.addMockFunction("getSelectedRange", () => contextMock.workbook.range);
 
-    context.workbook.range.setMock("address", "C2");
-    assert.strictEqual(await getSelectedRangeAddress(context), "C2");
+    contextMock.workbook.range.setMock("address", "C2");
+    assert.strictEqual(await getSelectedRangeAddress(contextMock), "C2");
   });
-  // it("run", async function () {
-  //   global.Excel = new ExcelMock() as any;
-  //   await run();
-  //   assert.strictEqual((global.Excel as any).context.workbook.range.format.fill.color, "yellow");
-  // });
+  it("run", async function () {
+    const excelMock = new OfficeJSMock("context") as any;
+
+    excelMock.addMockObject("context");
+    excelMock.context.addMockObject("workbook");
+    excelMock.context.workbook.addMockObject("range");
+    excelMock.context.workbook.addMockFunction("getSelectedRange", () => excelMock.context.workbook.range);
+    excelMock.context.workbook.range.setMock("address", "G4");
+    excelMock.context.workbook.range.addMockObject("format");
+    excelMock.context.workbook.range.format.addMockObject("fill");
+  
+    excelMock.addMockFunction("run", async function(callback) {
+      await callback(excelMock.context);
+    });
+    global.Excel = excelMock;
+    await run();
+
+    assert.strictEqual((global.Excel as any).context.workbook.range.format.fill.color, "yellow");
+  });
 });
