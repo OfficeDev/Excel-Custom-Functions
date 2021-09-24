78e6a5da49d565431fd9566eaf9d3b46a6603f32
Darren Miller <darrmill@microsoft.com>
2021-09-23 18:56:10 -0700

    Use specific image names

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -56,6 +56,9 @@ module.exports = async (env, options) => {
         {
           test: /\.(png|jpg|jpeg|gif|ico)$/,
           type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext][query]",
+          },
         },
       ],
     },
--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,9 @@ module.exports = async (env, options) => {
         {
           test: /\.(png|jpg|jpeg|gif|ico)$/,
           type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext][query]",
+          },
         },
       ],
     },
