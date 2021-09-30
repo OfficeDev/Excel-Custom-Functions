8079cad951a277ee10601b438a0460c2136e3ecf
igor-ribeiiro <igormr98mr@gmail.com>
2021-09-30 09:17:13 -0700

    PR comments

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -8,7 +8,9 @@ import * as runWord from "./word";
 // images references in the manifest
 import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
+import "../../assets/icon-64.png";
 import "../../assets/icon-80.png";
+import "../../assets/icon-128.png";
 
 /* global document, Office */
 
