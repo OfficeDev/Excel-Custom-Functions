4c0702190fb3d44c21ab14e51591a88227d850de
Darren Miller <darrmill@microsoft.com>
2021-10-06 19:16:53 -0700

    Add folder delete to convert script

--- convertToSingleHost.js
+++ convertToSingleHost.js
@@ -76,6 +76,9 @@ async function convertProjectToSingleHost(host) {
   // delete the .github folder
   deleteFolder(path.resolve(`./.github`));
 
+  // delete CI/CD pipeline files
+  deleteFolder(path.resolve(`./.azure-devops`));
+
   // delete repo support files
   await deleteSupportFiles();
 }
