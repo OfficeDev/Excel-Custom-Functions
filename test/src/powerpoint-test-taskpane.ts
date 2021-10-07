bdd82ad87738264d6c2a1e2a9fb142e9198a01c5
Darren Miller <darrmill@microsoft.com>
2021-10-06 18:35:08 -0700

    Linter fixes on test files

--- test/src/powerpoint-test-taskpane.ts
+++ test/src/powerpoint-test-taskpane.ts
@@ -8,35 +8,35 @@ const port: number = 4201;
 let testValues: any = [];
 
 Office.onReady(async (info) => {
-    if (info.host === Office.HostType.PowerPoint) {
-        const testServerResponse: object = await pingTestServer(port);
-        if (testServerResponse["status"] == 200) {
-            runTest();
-        }
+  if (info.host === Office.HostType.PowerPoint) {
+    const testServerResponse: object = await pingTestServer(port);
+    if (testServerResponse["status"] == 200) {
+      runTest();
     }
+  }
 });
 
 async function getSelectedText(): Promise<string> {
-    return new Promise((resolve, reject) => {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, (result: Office.AsyncResult<string>) => {
-            if (result.status === Office.AsyncResultStatus.Failed) {
-                reject(result.error);
-            } else {
-                resolve(result.value);
-            }
-        });
-    })    
+  return new Promise((resolve, reject) => {
+    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, (result: Office.AsyncResult<string>) => {
+      if (result.status === Office.AsyncResultStatus.Failed) {
+        reject(result.error);
+      } else {
+        resolve(result.value);
+      }
+    });
+  });
 }
 
 export async function runTest(): Promise<void> {
-    // Execute taskpane code
-    await run();
+  // Execute taskpane code
+  await run();
 
-    // get selected text
-    const selectedText = await getSelectedText();
+  // get selected text
+  const selectedText = await getSelectedText();
 
-    // send test results
-    testHelpers.addTestResult(testValues, "output-message", selectedText, "Hello World!");
+  // send test results
+  testHelpers.addTestResult(testValues, "output-message", selectedText, "Hello World!");
 
-    await sendTestResults(testValues, port);
+  await sendTestResults(testValues, port);
 }
