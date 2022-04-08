7a5e60c7761f003daeeecc0b36421824a98f21a1
Darren Miller <darrmill@microsoft.com>
2022-04-08 11:56:20 -0700

    Try repeated screenshots

--- test/end-to-end/src/test-taskpane.ts
+++ test/end-to-end/src/test-taskpane.ts
@@ -2,7 +2,7 @@ import * as functionsJsonData from "./test-data.json";
 import { pingTestServer, sendTestResults } from "office-addin-test-helpers";
 import { closeWorkbook, sleep } from "./test-helpers";
 
-/* global Office, document, Excel, run, console */
+/* global Office, document, Excel, run */
 const customFunctionsData = (<any>functionsJsonData).functions;
 const port: number = 4201;
 let testValues = [];
@@ -12,7 +12,7 @@ Office.onReady(async () => {
   document.getElementById("app-body").style.display = "flex";
   document.getElementById("run").onclick = run;
 
-  var testServerResponse: object = await pingTestServer(port);
+  const testServerResponse: object = await pingTestServer(port);
   if (testServerResponse["status"] === 200) {
     await runCfTests();
     await sendTestResults(testValues, port);
@@ -33,6 +33,27 @@ async function runCfTests(): Promise<void> {
 
       // Check to if this is a streaming function
       await readCFData(key, customFunctionsData[key].streaming != undefined ? 2 : 1);
+    }
+  });
+}
+
+export async function readCFData(cfName: string, readCount: number): Promise<void> {
+  await Excel.run(async (context) => {
+    // if this is a streaming function, we want to capture two values so we can
+    // validate the function is indeed streaming
+    for (let i = 0; i < readCount; i++) {
+      try {
+        const range = context.workbook.getSelectedRange();
+        range.load("values");
+        await context.sync();
+
+        await sleep(5000);
+
+        addTestResult(cfName, range.values[0][0]);
+        Promise.resolve();
+      } catch {
+        Promise.reject();
+      }
     }
   });
 }
@@ -60,25 +81,25 @@ async function runCfTests(): Promise<void> {
 //   });
 // }
 
-export async function readCFData(cfName: string, readCount: number): Promise<void> {
-  await Excel.run(async (context) => {
-    // if this is a streaming function, we want to capture two values so we can
-    // validate the function is indeed streaming
-    for (let i = 0; i < readCount; i++) {
-      try {
-        const range = context.workbook.getSelectedRange();
-        range.load("values");
-        await context.sync();
+// export async function readCFData(cfName: string, readCount: number): Promise<void> {
+//   await Excel.run(async (context) => {
+//     // if this is a streaming function, we want to capture two values so we can
+//     // validate the function is indeed streaming
+//     for (let i = 0; i < readCount; i++) {
+//       try {
+//         const range = context.workbook.getSelectedRange();
+//         range.load("values");
+//         await context.sync();
 
-        await sleep(5000);
+//         await sleep(5000);
 
-        addTestResult(cfName, range.values[0][0]);
-      } catch (err: any) {
-        console.log(`Error trying to get function value: ${err}`);
-      }
-    }
-  });
-}
+//         addTestResult(cfName, range.values[0][0]);
+//       } catch (err: any) {
+//         console.log(`Error trying to get function value: ${err}`);
+//       }
+//     }
+//   });
+// }
 
 function addTestResult(resultName: string, resultValue: any) {
   var data = {};
--- test/end-to-end/ui-test.ts
+++ test/end-to-end/ui-test.ts
@@ -7,8 +7,9 @@ import { pingTestServer } from "office-addin-test-helpers";
 import { closeDesktopApplication } from "./src/test-helpers";
 import * as officeAddinTestServer from "office-addin-test-server";
 import * as path from "path";
+const screenshot = require("screenshot-desktop");
 
-/* global process, describe, before, it, after, require, setTimeout, clearTimeout */
+/* global process, describe, before, it, after, require, setInterval, clearInterval */
 const host: string = "excel";
 const manifestPath = path.resolve(`${process.cwd()}/test/end-to-end/test-manifest.xml`);
 const port: number = 4201;
@@ -43,16 +44,17 @@ describe("Test Excel Custom Functions", function () {
       this.timeout(600000); // timeout after 15 min
 
       // After 10 min take a screenshot to look for problems.
+      let shotCount = 0;
       function getScreenShot() {
-        const screenshot = require("screenshot-desktop");
-        screenshot({ filename: "screen.jpg" });
+        shotCount++;
+        screenshot({ filename: `screen-${shotCount}.jpg` });
       }
-      let id = setTimeout(getScreenShot, 60000);
+      let interval = setInterval(getScreenShot, 10000);
 
       // Expecting six result values
       testValues = await testServer.getTestResults();
       assert.strictEqual(testValues.length, 6);
-      clearTimeout(id);
+      clearInterval(interval);
     });
     it("ADD function should return expected value", async function () {
       assert.strictEqual(testJsonData.functions.ADD.result, testValues[0].Value);
