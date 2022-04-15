1ee89abca21e5d096ec8f7904085d845169ae3c5
Darren Miller <darrmill@microsoft.com>
2022-04-15 14:42:43 -0700

    Switch to Excel.run

--- test/end-to-end/src/test-taskpane.ts
+++ test/end-to-end/src/test-taskpane.ts
@@ -14,7 +14,7 @@ Office.onReady(async () => {
 
   const testServerResponse: object = await pingTestServer(port);
   if (testServerResponse["status"] === 200) {
-    await new Promise<void>(() => {
+    Excel.run(async (context) => {
       addTestResult("ADD", "Nothing");
       Promise.resolve();
     });
