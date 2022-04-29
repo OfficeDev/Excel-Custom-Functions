6f124d577464e984646e34630ec243749da3c6ff
Darren Miller <darrmill@microsoft.com>
2022-04-29 11:49:53 -0700

    Try switching order of tests

--- test/end-to-end/tests.ts
+++ test/end-to-end/tests.ts
@@ -19,6 +19,48 @@ const testServer = new officeAddinTestServer.TestServer(port);
 let testValues: any = [];
 
 describe("Test Excel Custom Functions", function () {
+  describe("Debugger Tests", function () {
+    before(`Setup test environment and sideload ${host}`, async function () {
+      this.timeout(0);
+      // Call startDebugging to start dev-server and sideload
+      const devServerCmd: string = `npm run dev-server -- --config ./test/end-to-end/webpack.config.js --env testType=debugger`;
+      const devServerPort: number = 3001;
+      const options = {
+        appType: AppType.Desktop,
+        app: toOfficeApp(host),
+        devServerCommandLine: devServerCmd,
+        devServerPort: devServerPort,
+        enableDebugging: true,
+      };
+      await startDebugging(manifestPath, options);
+    });
+    describe("Test Debugger", function () {
+      let ws: WebSocket;
+      before("Open websocket connection to Debugger", async function () {
+        this.timeout(60 * 1000);
+        ws = await connectToWebsocket();
+        assert.notStrictEqual(ws, undefined, "Unable to connect to the websocket.");
+      });
+      it("enable debugging", async function () {
+        await enableDebugging(ws);
+      });
+      it("pause debugging", async function () {
+        await pauseDebugging(ws);
+      });
+      after("Close websocket connection", async function () {
+        ws.close();
+      });
+    });
+    after("Teardown test environment", async function () {
+      this.timeout(0);
+      // Close excel
+      const applicationClosed = await closeDesktopApplication();
+      assert.strictEqual(applicationClosed, true);
+
+      // Unregister the add-in
+      await stopDebugging(manifestPath);
+    });
+  });
   describe("UI Tests", function () {
     before(`Setup test environment and sideload ${host}`, async function () {
       this.timeout(0);
@@ -87,48 +129,6 @@ describe("Test Excel Custom Functions", function () {
       const stopTestServer = await testServer.stopTestServer();
       assert.strictEqual(stopTestServer, true);
 
-      // Close excel
-      const applicationClosed = await closeDesktopApplication();
-      assert.strictEqual(applicationClosed, true);
-
-      // Unregister the add-in
-      await stopDebugging(manifestPath);
-    });
-  });
-  describe("Debugger Tests", function () {
-    before(`Setup test environment and sideload ${host}`, async function () {
-      this.timeout(0);
-      // Call startDebugging to start dev-server and sideload
-      const devServerCmd: string = `npm run dev-server -- --config ./test/end-to-end/webpack.config.js --env testType=debugger`;
-      const devServerPort: number = 3001;
-      const options = {
-        appType: AppType.Desktop,
-        app: toOfficeApp(host),
-        devServerCommandLine: devServerCmd,
-        devServerPort: devServerPort,
-        enableDebugging: true,
-      };
-      await startDebugging(manifestPath, options);
-    });
-    describe("Test Debugger", function () {
-      let ws: WebSocket;
-      before("Open websocket connection to Debugger", async function () {
-        this.timeout(60 * 1000);
-        ws = await connectToWebsocket();
-        assert.notStrictEqual(ws, undefined, "Unable to connect to the websocket.");
-      });
-      it("enable debugging", async function () {
-        await enableDebugging(ws);
-      });
-      it("pause debugging", async function () {
-        await pauseDebugging(ws);
-      });
-      after("Close websocket connection", async function () {
-        ws.close();
-      });
-    });
-    after("Teardown test environment", async function () {
-      this.timeout(0);
       // Close excel
       const applicationClosed = await closeDesktopApplication();
       assert.strictEqual(applicationClosed, true);
