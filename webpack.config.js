441556f80973999f313e2d41fe652236978a204d
Darren Miller <darrmill@microsoft.com>
2022-08-19 13:56:57 -0700

    Update webpack map file output

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ module.exports = async (env, options) => {
       commands: "./src/commands/commands.ts",
     },
     output: {
-      devtoolModuleFilenameTemplate: "webpack:///[resource-path]?[loaders]",
       clean: true,
     },
     resolve: {
