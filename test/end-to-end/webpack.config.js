906d9d5c12c4c381869b539db58a89e3b1dd64c2
Darren Miller <darrmill@microsoft.com>
2022-11-08 15:37:30 -0800

    Update static file path

--- test/end-to-end/webpack.config.js
+++ test/end-to-end/webpack.config.js
@@ -20,8 +20,8 @@ module.exports = async (env, options) => {
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
       commands: "./src/commands/commands.ts",
-      functions: "./src/functions/functions.ts",
       taskpane: "./test/end-to-end/src/test-taskpane.ts",
+      functions: "./src/functions/functions.ts",
     },
     output: {
       path: path.resolve(__dirname, "testBuild"),
@@ -96,7 +96,10 @@ module.exports = async (env, options) => {
       }),
     ],
     devServer: {
-      static: ["./"],
+      static: {
+        directory: path.resolve("./", "dist"),
+        publicPath: "/public",
+      },
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
