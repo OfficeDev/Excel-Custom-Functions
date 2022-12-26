33dba34d77d0b63515a3b422031be1fd483b358a
sunsetXiao <quanfuxiao@microsoft.com>
2022-12-26 11:17:13 +0800

    Fix webpack html hot-update

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = async (env, options) => {
     devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      taskpane: "./src/taskpane/taskpane.ts",
+      taskpane: ["./src/taskpane/taskpane.ts", "./src/taskpane/taskpane.html"],
       commands: "./src/commands/commands.ts",
     },
     output: {
