196a82991ed268b3d98969bd224888b5c44c8bcc
Adam Krantz <adamk@microsoft.com>
2024-01-25 14:48:33 -0800

    don't use /public path with shared runtime
    The shared runtime uses the browser context for execution of JavaScript custom functions,
    so the functions.json metadata can come from the dist folder just like the other files.

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,7 @@ module.exports = async (env, options) => {
               if (dev) {
                 return content;
               } else {
-                return content.toString().replace(new RegExp(urlDev + "(?:public/)?", "g"), urlProd);
+                return content.toString().replace(urlDev, urlProd);
               }
             },
           },
